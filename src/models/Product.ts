bd6957fab7fedc417d14c2c14db0564336b8c245
heorhii_podliehaiev <heorhii_podliehaiev@epam.com>
2022-07-15 22:47:04 +0300

    added BE api and updated product schema

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,10 +5,12 @@ export type Product = {
   title: string,
   description: string,
   price: number,
+  imageUrl: string,
 };
 
 export const ProductSchema = Yup.object().shape({
   title: Yup.string().required(),
   description: Yup.string(),
+  imageUrl: Yup.string(),
   price: Yup.number().required(),
 });
