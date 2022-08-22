52ca52c8c66909f84821494a10eff8e91d5f0d87
heorhii_podliehaiev <heorhii_podliehaiev@epam.com>
2022-08-23 02:03:56 +0300

    added cart link from deployed cart-service application

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://sdwoei8t06.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://4vq1uhh29b.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://gpodliegaiev-cart-api-dev.eu-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
